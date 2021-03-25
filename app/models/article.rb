8d3c6d42613d365eb6d36a3098e4af67c44b17ea
Vincenzo <vinzgameslol@gmail.com>
2021-03-25 22:16:02 +0100

    Solved conflicts

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,9 +1,6 @@
 class Article < ApplicationRecord
-<<<<<<< HEAD
     has_many :comments 
     
-=======
->>>>>>> main
     validates :title, presence: true 
     validates :body, presence: true, length: { minimum: 10 }
 end
